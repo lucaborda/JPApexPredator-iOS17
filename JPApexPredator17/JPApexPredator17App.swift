947bfb3466ee2c8035fe2874b6f933ebaebf52e9
luca borda <lucaborda11@gmail.com>
2024-07-26 18:35:45 -0300

    Initial Commit

---
+++ JPApexPredator17/JPApexPredator17App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredator17App.swift
+//  JPApexPredator17
+//
+//  Created by Luca Borda on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredator17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
